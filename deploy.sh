eea8a767111bde77cb5dc3f83b4e0517702422a2
David Skinner <skinner.david@gmail.com>
2018-10-12 11:25:58 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "\033[0;32mDeploying Huny-B.CBD-Oil.App updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t davsk-hugo-theme
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
